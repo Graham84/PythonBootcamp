3ab1c1edf89942020ef9d8f79d97b9175395fa21
Graham84 <gosiochain@rocketmail.com>
2017-07-25 22:09:00 +0100

    25-07-17
    Milestone Project 2 - Blackjack
    Modules, Packages and imports

---
+++ 9 - Modules and Packages/2 - Create your own Python modules.py
@@ -0,0 +1,26 @@
+# Create your own Python modules.
+# Section 11, Lecture 68
+# A module is a file containing Python definitions and statements.
+# The file name is the module name with the suffix .py
+#
+# create hello.py then write the following function:
+#
+# def helloworld():
+#
+# print "hello"
+#
+# >>> import hello
+#
+# >>> hello.helloworld()
+#
+# 'hello'
+#
+# >>>
+#
+# To group many .py files put them in a folder.
+# Any folder with an __init__.py is considered a module by python
+# and you can call them a package
+#
+# |-HelloModule
+#
+# |_ __init__.py
